4b720e91a77f88d35d213757d597dc9bcd5ce4c3
kpavan <kpavan@smartzip.com>
2018-03-04 23:01:20 +0530

    exercisep-12 http uppercase server

--- myModule.js
+++ myModule.js
@@ -4,7 +4,7 @@ var options = {
     hostname: 'localhost',
     port: 8000, // update this to port where server is started
     path: '/',
-    method: 'GET'
+    method: 'POST'
 };
 
 var request = http.request(options, function name(resp) {
@@ -12,6 +12,9 @@ var request = http.request(options, function name(resp) {
         console.log(chunk.toString());
     })
 })
+
+var postData = "hello world";
+request.write(postData);
 
 request.on('error', function(e) {
     console.log('problem with request: ' + e.message);
--- node-program.js
+++ node-program.js
@@ -1,12 +1,17 @@
 const http = require('http');
-const fs = require('fs');
+const mapThrough = require('through2-map');
 
 var port = process.argv[2];
-var filePath = process.argv[3];
 
 var server = http.createServer(function (req, res) {
-    res.writeHead(200, { 'content-type': 'text/plain' })
-    var data = fs.createReadStream(filePath)
-    data.pipe(res)
+    if (req.method == 'POST') {
+        req.pipe(mapThrough(function (chunk) {
+            return chunk.toString().toUpperCase();
+        })).pipe(res)
+    }else{
+        res.write('send me a post request \n');
+        res.end();
+    }
+
 })
 server.listen(port);
